a2caa70d546e9efef8ce4b5a3fae3a6963f38d2c
Aidan Hunt <aidanhunt@acm.org>
2023-05-07 20:33:58 -0400

    minimal express app

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http');
+import minimist from 'minimist'
+const args = minimist(process.argv.slice(2)); // slice(2) removes two dummy arguments
+const port = (args.port || process.env.PORT || 3000)
 
-const minimist = require('minimist')(process.argv.slice(2)); // slice(2) removes two dummy arguments
+import express from 'express'
+const app = express()
 
-const port = (minimist['port'] || process.env.PORT || 3000)
 
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
