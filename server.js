25bf411dcc8d9e8659d880ff153ce03f3cb6a980
Aidan Hunt <aidanhunt@acm.org>
2023-05-07 20:41:41 -0400

    add 404 per express docs

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express()
 
 
 
+// handle requests not matching any route
+app.use((req, res, next) => 
+{
+  res.set('Content-Type', 'text/plain')
+  res.status(404).send("404 NOT FOUND")
+})
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
